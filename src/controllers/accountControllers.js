4de7e4bda8688bf3ff69c0eaf55eec3a79dd83cc
lostzilla <alanaa92@gmail.com>
2022-02-12 17:25:38 -0300

    feat: account page

--- src/controllers/accountControllers.js
+++ src/controllers/accountControllers.js
@@ -17,3 +17,78 @@ export async function getUserInformation(req, res) {
 
 
 }
+
+export async function getUserPayment(req, res) {
+
+  try {
+
+    const user = res.locals.user;
+
+    res.send(user.payment);
+
+  } catch {
+    res.sendStatus(500)
+  }
+
+
+}
+
+export async function setPaymentMethod(req, res) {
+
+  const { id } = req.params;
+  const paymentMethod = req.body;
+
+  const user = res.locals.user;
+
+  const newUser = { ...user, ...paymentMethod }
+
+
+  try {
+
+    await db.collection("users").updateOne({ _id: new ObjectId(id) },
+      { $set: newUser }
+    )
+
+    res.sendStatus(200);
+
+  } catch {
+    res.sendStatus(500)
+  }
+}
+
+export async function getUserAddress(req, res) {
+
+  try {
+
+    const user = res.locals.user;
+
+    res.send(user.address);
+
+  } catch {
+    res.sendStatus(500)
+  }
+
+
+}
+
+export async function setUserAddress(req, res) {
+
+  const { id } = req.params;
+  const address = req.body;
+
+  const user = res.locals.user;
+
+  const newUser = { ...user, "address": address }
+
+  try {
+
+    await db.collection("users").updateOne({ _id: new ObjectId(id) },
+      { $set: newUser }
+    )
+
+    res.sendStatus(200);
+
+  } catch {
+    res.sendStatus(500)
+  }
+}
--- src/routes/accountRouter.js
+++ src/routes/accountRouter.js
@@ -1,11 +1,16 @@
 import express from "express";
 import { validateBearerToken } from "../middlewares/validateBearerToken.js";
 import { getUserInformation } from "../controllers/accountControllers.js";
+import { getUserPayment, setPaymentMethod, getUserAddress, setUserAddress } from "../controllers/accountControllers.js";
+import { } from "../controllers/accountControllers.js";
 const accountRouter = express.Router();
 
 
 accountRouter.use(validateBearerToken)
 accountRouter.get("/soundbytes/account", getUserInformation);
-
+accountRouter.get("/soundbytes/account/payment/:id", getUserPayment)
+accountRouter.put("/soundbytes/account/payment/:id", setPaymentMethod)
+accountRouter.get("/soundbytes/account/address/:id", getUserAddress)
+accountRouter.put("/soundbytes/account/address/:id", setUserAddress)
 
 export default accountRouter;
